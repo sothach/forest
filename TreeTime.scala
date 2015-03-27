cec395ae64bcd21b1b07b1fae5acb3c52fb6da89
sothach <phillips.roy@gmail.com>
2015-03-27 15:36:23 +0000

    Update TreeTime.scala

--- TreeTime.scala
+++ TreeTime.scala
@@ -1,4 +1,4 @@
-import oracle.net.aso.{n, h}
+import oracle.net.aso.{i, n, h}
 import org.nulleins.trees.{Empty, RedBlackTree}
 import org.scalatest._
 
@@ -40,33 +40,33 @@ class TreeTime extends FlatSpec with Matchers {
 
   implicit def setConvert[A](tree: RedBlackTree[A]): Set[A] = tree.iterator.toSet
 
+  /** @return the height of a ''perfect'' binary tree of with #`elements` */
   def height(elements: Int): Int = {
     def modp2(size: Int): Int = {
         val h = List(1, 2, 4, 8, 16).foldLeft(size - 1)((v, e) => v | (v >> e)) + 1
         if (h == size) modp2(h + 1) else h
     }
-    if(elements <= 0) 0
-    else if(elements == 1) 1
+    if(elements < 2) elements
     else ceil(log(modp2(elements)) / log(2)).toInt
   }
 
   /*
-  1:                         1
-  2:                       2   3
-  3:                    4  5   6  7
-  4:               8 9 10 11  12 13 14 15
-  5: 16 17 18 19 20 21 22 23  24 25 26 27 28 29 30 31
-                              8
-                      4                  12
-                  2      6         10          16
-                1  3   5   7     9   11    14       18
-                                         13  15   17  19
-                                                         20
+  1:                         1                        |              8
+  2:                       2   3                      |       4                 12
+  3:                    4  5   6  7                   |   2      6        10          16
+  4:               8 9 10 11  12 13 14 15             | 1  3   5   7    9   11    14       18
+  5: 16 17 18 19 20 21 22 23  24 25 26 27 28 29 30 31 |                          13  15   17  19
+  6:                                                  |                                          20
   */
   "Tree metrics" should "be calculated (e.g, O(logn)?" in {
     val res = (1 to 33) map ( n => (n,height(n)))
     println(res)
   }
 
-}
+  "bitwise mod pow 2" should "work" in {
+    println(s"${sqrt(32)} ${pow(2,5)}")
+    val res = for(i <- Iterator.iterate(1)(pow(2,_).toInt) takeWhile(_ <= 16)) yield i
+    println(res.toList)
+  }
 
+}
