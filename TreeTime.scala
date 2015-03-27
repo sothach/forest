e14ceeeb79ab1a31360010ff57099a3f949beb64
sothach <phillips.roy@gmail.com>
2015-03-27 14:55:03 +0000

    Update TreeTime.scala

--- TreeTime.scala
+++ TreeTime.scala
@@ -1,47 +1,72 @@
+import oracle.net.aso.{n, h}
 import org.nulleins.trees.{Empty, RedBlackTree}
-import org.scalatest.FunSuite
+import org.scalatest._
 
-class TreeTime extends FunSuite {
-  test("red-black tree") {
-    val tree = (1 to 20).foldLeft(RedBlackTree(1))(_+_)
+import scala.math._
+
+class TreeTime extends FlatSpec with Matchers {
+
+  "A RB Tree" should "have the correct size" in {
+    val tree = (1 to 20).foldLeft(RedBlackTree(1))(_ + _)
     tree.show()
-    assert(tree.size() === 20)
-    assert(tree.depth() === 6)
+    tree.size() shouldBe 20
+    tree.depth() shouldBe 6
   }
 
-  test("word tree") {
+  "A word tree" should "be sorted correctly" in {
     val tree = RedBlackTree("Dublin", "Cork", "Limerick", "Sligo", "Belfast",
-                "Waterford", "Wexford", "Athlone", "Edenderry", "Finglas", "Galway")
-    tree.show(Console.out)
-    tree.iterate() foreach println
-    assert(tree.size() === 11)
+      "Waterford", "Wexford", "Athlone", "Edenderry", "Finglas", "Galway")
+    tree.iterator.toStream shouldBe sorted
+    tree.toSet should contain ("Dublin")
+    tree.show()
+    tree.size() shouldBe 11
 
-    assert(tree.find("New Ross").isEmpty)
-    assert(tree.find("Wexford").isDefined)
+    tree.find("New Ross") shouldBe empty
+    tree.find("Wexford") should not be empty
 
     val list = tree.find("Edenderry") match {
-      case None => ""
-      case Some(root) => root.to().toList map identity
+      case None => Nil
+      case Some(root) => root.to.toList map identity
     }
-    println(list)
+    list.size shouldBe 5
   }
 
-  test("building") {
-    val tree = "efg" :: "hij" :: "bcd" :: Empty
+  "Tree builders" should "correctly concatenate trees" in {
+    val tree = "aaa" :: "efg" :: "hij" :: "bcd" :: Empty
     val tree2 = tree :: RedBlackTree("klm", "nop", "qrs", "tuv", "wxy", "zzz")
-    tree2.show()
-    assert(tree2 contains "bcd")
-    assert(tree2 contains "klm")
+    tree2.toSet should contain ("bcd")
+    tree2.toSet should contain ("klm")
   }
 
-  /*  test("O(logn)?") {
-      def log2(n: Int) = {
-        require(n > 0)
-        math.log(n) / math.log(2)
-      }
-      val n = 20
-      println(s"n=$n logn=${math.log(n)}")
-      println(s"2^h (h=$n) = ${math.pow(2,n)}")
-      println(s"log2(n+1)) = ${log2(n+1)}")
-    }*/
+  implicit def setConvert[A](tree: RedBlackTree[A]): Set[A] = tree.iterator.toSet
+
+  def height(elements: Int): Int = {
+    def modp2(size: Int): Int = {
+        val h = List(1, 2, 4, 8, 16).foldLeft(size - 1)((v, e) => v | (v >> e)) + 1
+        if (h == size) modp2(h + 1) else h
+    }
+    if(elements <= 0) 0
+    else if(elements == 1) 1
+    else ceil(log(modp2(elements)) / log(2)).toInt
+  }
+
+  /*
+  1:                         1
+  2:                       2   3
+  3:                    4  5   6  7
+  4:               8 9 10 11  12 13 14 15
+  5: 16 17 18 19 20 21 22 23  24 25 26 27 28 29 30 31
+                              8
+                      4                  12
+                  2      6         10          16
+                1  3   5   7     9   11    14       18
+                                         13  15   17  19
+                                                         20
+  */
+  "Tree metrics" should "be calculated (e.g, O(logn)?" in {
+    val res = (1 to 33) map ( n => (n,height(n)))
+    println(res)
+  }
+
 }
+
